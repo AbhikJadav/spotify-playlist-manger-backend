320f30693dfdf8252155324a308845ef11842603
AbhikJadav <abhik.jadav002@gmail.com>
2025-01-30 21:44:08 +0530

    fix:cors error

--- src/middleware/corsMiddleware.ts
+++ src/middleware/corsMiddleware.ts
@@ -2,8 +2,7 @@ import cors from 'cors';
 
 export const corsMiddleware = cors({
     origin: '*',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
-    exposedHeaders: ['Content-Range', 'X-Content-Range']
+    methods: '*',
+    allowedHeaders: '*',
+    credentials: false
 });
