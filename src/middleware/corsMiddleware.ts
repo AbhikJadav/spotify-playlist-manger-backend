7dbcd280035020014fe5094d6b6382b590dc65e0
AbhikJadav <abhik.jadav002@gmail.com>
2025-01-30 19:49:48 +0530

    add change to corrs middleware

--- src/middleware/corsMiddleware.ts
+++ src/middleware/corsMiddleware.ts
@@ -1,18 +1,9 @@
 import cors from 'cors';
 
-const allowedOrigins = [
-    'http://localhost:3000',
-    'http://localhost:5173',
-    'https://spotify-playlist-manager.vercel.app',
-    'https://spotify-playlist-manager-git-main.vercel.app',
-    'https://spotify-playlist-manager-frontend-umber.vercel.app',
-    'https://spotify-playlist-manger-backend-jvdysbc1t.vercel.app'
-];
-
 export const corsMiddleware = cors({
-    origin: allowedOrigins,
+    origin: true,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
     exposedHeaders: ['Content-Range', 'X-Content-Range']
-});
+});ss
