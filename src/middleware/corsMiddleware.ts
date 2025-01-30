4ea944db1f040c6d3ed5e9ff2b84af6ed981a37a
AbhikJadav <48055286+AbhikJadav@users.noreply.github.com>
2025-01-30 21:06:21 +0530

    Update corsMiddleware.ts

--- src/middleware/corsMiddleware.ts
+++ src/middleware/corsMiddleware.ts
@@ -1,7 +1,7 @@
 import cors from 'cors';
 
 export const corsMiddleware = cors({
-    origin: true,
+    origin: '*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
